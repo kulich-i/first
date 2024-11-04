2b83100fe08989a8df7525e1ed39dfd324f9afe1
Kulich-i <zukovskijila349@gmail.com>
2024-11-05 02:41:05 +0300

    first commit

---
+++ myfirst/myfirst/apps/articles/models.py
@@ -0,0 +1,31 @@
+import datetime
+from django.db import models
+
+from django.utils import timezone
+
+class Article(models.Model):
+    article_title = models.CharField('название', max_length = 200)
+    article_text = models.TextField('текст')
+    pub_date = models.DateTimeField('дата')
+
+    def __str__(self):
+        return self.article_title
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days = 7))
+
+    class Meta:
+        verbose_name = 'текст'
+        verbose_name_plural = 'много текста'
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+    author_name = models.CharField('имя', max_length = 50)
+    comment_text = models.CharField('комментарий', max_length = 200)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'много Комментариев'
